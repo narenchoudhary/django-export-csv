0d36ad9657ef9935eb776202785a00e8aaf6f331
Narendra Choudhary <narendra.prog@gmail.com>
2016-11-27 18:23:02 +0530

    Add more tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,6 +2,7 @@ import mock
 
 from django.http import HttpResponse
 from django.test import RequestFactory, TestCase
+from django.utils import timezone
 
 from export_csv.exceptions import NoModelFoundException
 from export_csv.views import ExportCSV
@@ -12,7 +13,10 @@ from .models import Customer
 class ExportCSVTests(TestCase):
 
     def setUp(self):
-        pass
+        Customer.objects.create(name='name1', address='address1',
+                                is_active=True, last_updated=timezone.now())
+        Customer.objects.create(name='name2', address='address2',
+                                is_active=True, last_updated=timezone.now())
 
     def setup_view(self, view, request, model=None, field_names=None,
                    filename=None, add_col_names=False, col_names=None,
@@ -89,31 +93,43 @@ class ExportCSVTests(TestCase):
                                              'last_updated']
         result_verbose_names = view._get_field_verbose_names()
         self.assertEqual(set(expected_verbose_names),
-                          set(result_verbose_names))
+                         set(result_verbose_names))
 
-    def test_get_col_names_model(self):
+    @mock.patch('export_csv.views.ExportCSV.get_field_names')
+    def test_get_field_verbose_names_exception(self, mock_get_field_names):
+        request = RequestFactory().get("")
+        view = ExportCSV()
+        view = self.setup_view(view, request, model=None)
+        mock_get_field_names.return_value = None
+        self.assertRaises(NoModelFoundException, view._get_field_verbose_names)
+
+    @mock.patch('export_csv.views.ExportCSV._get_field_verbose_names')
+    def test_get_col_names_model(self, mock_get_field_verbose_names):
         request = RequestFactory().get("")
         view = ExportCSV()
         expected_col_names = ['name', 'address', 'Is Active',
                                   'last updated']
         view = self.setup_view(view, request, model=Customer)
+        mock_get_field_verbose_names.return_value = expected_col_names
         result_col_names = view.get_col_names()
         self.assertEqual(set(expected_col_names), set(result_col_names))
 
-    def test_get_col_names_custom(self):
+    @mock.patch('export_csv.views.ExportCSV._get_field_verbose_names')
+    def test_get_col_names_custom(self, mock_get_field_verbose_names):
         request = RequestFactory().get("")
         view = ExportCSV()
         expected_col_names = ['name', 'address', 'Is Active',
                               'last updated']
         view = self.setup_view(view, request, model=Customer,
                                col_names=expected_col_names)
+        mock_get_field_verbose_names.return_value = None
         result_col_names = view.get_col_names()
         self.assertEqual(set(expected_col_names), set(result_col_names))
 
     def test_get_col_names_exception(self):
         request = RequestFactory().get("")
         view = ExportCSV()
-        view = self.setup_view(view, request, col_names="hello world")
+        view = self.setup_view(view, request, col_names="string")
         self.assertRaises(TypeError, view.get_col_names)
 
     def test_get_filename_custom(self):
@@ -146,12 +162,34 @@ class ExportCSVTests(TestCase):
         self.assertEqual(200, response.status_code)
         self.assertEqual('text/csv', response['Content-Type'])
 
+    def test_create_csv_col_names(self):
+        request = RequestFactory().get("")
+        view = ExportCSV()
+        view = self.setup_view(view, request, model=Customer,
+                               add_col_names=True)
+        response = view._create_csv()
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('text/csv', response['Content-Type'])
+
+    def clean_name(self, value):
+        return str(value).upper()
+
+    def test_create_csv_col_names_clean_name(self):
+        request = RequestFactory().get("")
+        view = ExportCSV()
+        view = self.setup_view(view, request, model=Customer,
+                               add_col_names=True)
+        view.clean_name = self.clean_name
+        response = view._create_csv()
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('text/csv', response['Content-Type'])
+
     @mock.patch('export_csv.views.ExportCSV._create_csv')
     def test_get(self, mock_create_csv):
         request = RequestFactory().get("")
         view = ExportCSV()
         view = self.setup_view(view, request, model=Customer)
-        mock_create_csv.return_value= HttpResponse(status=200, content_type='text/csv')
+        mock_create_csv.return_value = HttpResponse(status=200, content_type='text/csv')
         response = view.get(request=request)
         self.assertEqual(200, response.status_code)
         self.assertEqual('text/csv', response['Content-Type'])
